e6111d8c8027b93bf672cee34d86fc7414f5ebc1
fadiabji <fadi.abji@htomail.com>
2023-09-01 09:38:27 +0200

    Communication Between Components Overview, Component Parameters and Route Parameters

---
+++ DataBining/Client/BaseComponents/DateComponentBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DataBining.Client.BaseComponents
+
+{
+    public class DateComponentBase : ComponentBase
+    {
+
+        public DateTime dateTime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            dateTime = DateTime.Now;
+            base.OnInitialized();
+        }
+    }
+}
